feabb1df3a50a6d734fbf0d45ba57094c64cc096
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-12-30 16:48:56 -0800

    Final Commit2 I forgot to populate the vector the first time, that was kinda the whole point of the program silly me

--- multiplyVectors/main.cpp
+++ multiplyVectors/main.cpp
@@ -1,19 +1,26 @@
-//Abel Anand
+//  Abel Anand
 
 #include <iostream>
 #include <vector>
 #include <time.h>
 using namespace std;
 
-vector<int> nums;
+
+void printVector(vector <int> const &a, int scalar) {
+    cout << "Vector values multiplied by " << scalar << endl;
+    for(int i=0; i < a.size(); i++){
+   cout << a.at(i) * scalar << endl;
+}
+}
 
 int main() {
+    vector<int> nums;
     srand(time(0));
     int scalar;
     cout << "Enter a number you want to multiply to 50 random numbers\n";
     cin >> scalar;
     for (int i=0; i<=50; i++) {
-        cout << ((rand()%100)+1) * scalar << endl;
+        nums.push_back((rand()%100)+1);
     }
+    printVector(nums, scalar);
 }
- 
